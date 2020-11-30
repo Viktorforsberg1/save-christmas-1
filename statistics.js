1b81def1e67c1777e0d99b777ce06c4630bbd841
Viktor Forsberg <viktor.forsberg@skola.botkyrka.se>
2020-11-30 14:50:16 +0100

    del2

--- statistics.js
+++ statistics.js
@@ -1,12 +1,21 @@
 
 function max(arr) {
-
+    return Math.max(...array);
 }
 
 function min(arr) {
-
+    return Math.min(...array);
 }
 
-function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+function mean(arr, numbers) {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum / array.length
 }
+
+//const array = [3, 5, 7, 4, 3, 6, 2]
+//console.log(mean())
+//console.log(min())
+//console.log(max())
